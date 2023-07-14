ebd88e078549be388951cacc047e2ffbed2dd62c
bincymariambiju <bincymariambiju953@gmail.com>
2023-07-14 12:15:21 +0530

    reservertion

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "anjali@1998",
+    PASSWORD: "bincymariambiju",
     DB: "reservation",
     dialect: "mysql",
     pool: {
